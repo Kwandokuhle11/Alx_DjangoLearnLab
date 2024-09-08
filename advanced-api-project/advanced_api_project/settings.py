5db2d39e787fc3130c9ef2b4029e39cffe755890
Kwandokuhle11 <lwethukwando23@gmail.com>
2024-09-08 19:12:32 +0200

    add advanced api project

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -127,3 +127,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
+
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
