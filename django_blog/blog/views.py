19051dae899a95626a0406ddd984d42970fe1f21
Kwandokuhle11 <lwethukwando23@gmail.com>
2024-09-12 03:10:55 +0200

    blog

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,20 +1,40 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from .forms import RegisterForm
+from django.contrib.auth.models import User
+from django import forms
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
 
 def register(request):
     if request.method == 'POST':
-        form = RegisterForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  # Automatically log in the user after registration
-            return redirect('profile')
+            login(request, user)
+            return redirect('blog-home')
     else:
-        form = RegisterForm()
+        form = UserRegisterForm()
     return render(request, 'blog/register.html', {'form': form})
 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserChangeForm
+
+@login_required
 def profile(request):
-    return render(request, 'blog/profile.html')
+    if request.method == 'POST':
+        form = UserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = UserChangeForm(instance=request.user)
+    return render(request, 'blog/profile.html', {'form': form})
