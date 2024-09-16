e2484d465e3936384b7ef53570c4aafdb42949a6
Kwandokuhle11 <lwethukwando23@gmail.com>
2024-09-16 14:53:37 +0200

    posts

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets, permissions
+from .models import Post
+from .serializers import PostSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+from rest_framework import viewsets
+from .models import Comment
+from .serializers import CommentSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
