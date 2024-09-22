3e33a8aa547c3c23349de49e1cb0d9a95d598464
Kwandokuhle11 <lwethukwando23@gmail.com>
2024-09-22 22:18:55 +0200

    api

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -2,26 +2,29 @@ from argparse import Action
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics, status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework.authtoken.models import Token
-from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+from .models import CustomUser
+from .serializers import UserSerializer
 
-from accounts.models import CustomUser
-from .serializers import RegisterSerializer
+# Follow View
+class FollowUserView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = CustomUser.objects.all()
-    permission_classes = [permissions.IsAuthenticated]
+    def post(self, request, *args, **kwargs):
+        user_to_follow = get_object_or_404(CustomUser, id=kwargs['user_id'])
+        request.user.follow(user_to_follow)
+        return Response({"status": f"You are now following {user_to_follow.username}"}, status=status.HTTP_200_OK)
 
-    @Action(detail=True, methods=['post'])
-    def follow(self, request, pk=None):
-        user = self.get_object()
-        request.user.follow(user)
-        return Response({'status': f'You are now following {user.username}'})
+# Unfollow View
+class UnfollowUserView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer
 
-    @Action(detail=True, methods=['post'])
-    def unfollow(self, request, pk=None):
-        user = self.get_object()
-        request.user.unfollow(user)
-        return Response({'status': f'You have unfollowed {user.username}'})
+    def post(self, request, *args, **kwargs):
+        user_to_unfollow = get_object_or_404(CustomUser, id=kwargs['user_id'])
+        request.user.unfollow(user_to_unfollow)
+        return Response({"status": f"You have unfollowed {user_to_unfollow.username}"}, status=status.HTTP_200_OK)
