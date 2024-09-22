bf869f594566ee85d559c792c079916cf9a939e6
Kwandokuhle11 <lwethukwando23@gmail.com>
2024-09-22 22:55:40 +0200

    api

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -9,7 +9,7 @@ from social_media_api import posts
 class Post(models.Model):
     post = models.ForeignKey(posts, related_name='comments', on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -27,3 +27,10 @@ class Comment(models.Model):
     def __str__(self):
         return f'Comment by {self.author} on {self.post}'
         
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    liked_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')
